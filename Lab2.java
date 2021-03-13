8f6cc1165ed5c5906062c83b2343eb980cb738c1
John Henry <johnhmejia02@gmail.com>
2021-03-13 16:51:39 -0600

    Add main process method, update testfile

--- Lab2.java
+++ Lab2.java
@@ -8,7 +8,11 @@ public class Lab2 extends JFrame implements ActionListener {
 	JTextArea result = new JTextArea(20,40);
 	JLabel errors = new JLabel();
 	JScrollPane scroller = new JScrollPane();
-	
+
+	//This is a HashTable Dictionary with all of the variables. (Added by me :D)
+	Hashtable<String, String> hashVars = new Hashtable<String, String>();
+
+
 	public Lab2() {
 		setLayout(new java.awt.FlowLayout());
 		setSize(500,430);
@@ -47,26 +51,124 @@ public class Lab2 extends JFrame implements ActionListener {
 /* However, you should not change anything in the code that I have      */
 /* written.                                                             */
 /************************************************************************/
+	
+	
 
+
+
+	
 	public void processProgram()
 	{
+		
+		
+
+
 		String toProcess = getFileName();
 		System.out.println("Got the file name");
+
+		/*File readFile = new File(toProcess);
+
+		Scanner sc = new Scanner(readFile);
+		while (sc.hasNextLine())
+		{
+			System.out.println(sc.nextLine());
+		}
+		*/
 		System.out.println(toProcess);
 		System.out.println("printed toProcess");
 		//To do: get instructions text from readmetxt. 
-		result.setText(processTime(toProcess));
+		/*
+		for line in readmetext 
+		*/
+		
 
 	}
+
+
+
+	public String getVariable(String variableName)
+	{
+		return "hi";
+	}
+	public void setVariable(String variableName, String value)
+	{
+		
+	}
+
 	/**
 	 * 
-	 * @param instructions This is a line of code using the BASIC-style code givn. 
-	 * @return This returns the thing to print. 
+	 * @param instructions This is a line of code split into different words, and each word is an element in an arraylist
+	 * @param currentIndex The current index the code
+	 * @return This returns the next index to go to. In the case of END, it returns -1, which will terminate the program in another function
 	 */
-	public String processTime(String instructions){
-		return "Test Code";
-		//To do
-	}
+	public int processTime(ArrayList<String> instructions, int currentIndex){
 
+		if (instructions.get(0).equals("END"))
+		{
+			return -1;
+		}
+		else if (instructions.get(0).equals("GOTO"))
+		{
+			return Integer.parseInt(instructions.get(1));
+		}
+		else if (instructions.get(0).equals("PRINT"))
+		{
+			result.setText(result.getText() + hashVars.get(instructions.get(1)));
+			return currentIndex + 1;
+		}
+		
+		else if (instructions.get(0).equals("IF"))
+		{
+			boolean isTrue = false;
+			//TO DO. need to do a lot of exceptions and stuff 
+			
+			result.setText(result.getText() + hashVars.get(instructions.get(0)));
+			
+			
+			if (isTrue)
+			{
+				ArrayList<String> tempInstructions = new ArrayList<String>();
+
+				for (int i = 5; i < instructions.size(); i++)
+				{
+					tempInstructions.add(instructions.get(i));
+				}
+				return simpleStatementGetter(tempInstructions,currentIndex);
+			}
+			else {
+				return currentIndex + 1;
+			}
+			
+		}
+		else if (instructions.get(1).equals("=")){
+			hashVars.put(instructions.get(0),instructions.get(2));
+			return currentIndex + 1;
+		}
+		else {
+			//SET TO INVALID INDEX
+			return -1;
+		}
+		
+	}
+	public int simpleStatementGetter(ArrayList<String> instructions, int currentIndex)
+	{
+		if (instructions.get(0).equals("GOTO"))
+		{
+			return Integer.parseInt(instructions.get(1));
+		}
+		else if (instructions.get(0).equals("PRINT"))
+		{
+			result.setText(result.getText() + hashVars.get(instructions.get(1)));
+			return currentIndex + 1;
+		}
+		else if (instructions.get(1).equals("=")){
+			hashVars.put(instructions.get(0),instructions.get(2));
+			return currentIndex + 1;
+		}
+		else {
+			//SET TEXT TO INVALID INPUT
+			return -1;
+		}
+	}
 
 }
